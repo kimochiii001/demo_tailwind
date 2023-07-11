8c131593a2db47ba1a5e172a3ff025efce994dca
hdnhattan <19t1021226@husc.edu.vn>
2023-07-11 22:15:55 +0700

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Slider from '../components/Slider'
+import Header from '../components/Header'
+import Story from '../components/Story'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Slider/>
+        <Story/>
+    </div>
+  )
+}
+
+export default Home
